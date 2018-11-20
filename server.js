5a3809498e388083613b909e8da5a05b016c32b3
SamuelJ <samuel.jimoh@outlook.com>
2018-11-20 11:18:42 -0500

    added database connection

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
