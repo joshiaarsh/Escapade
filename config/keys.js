1478f06d7d0a65097605abeeb86c4bb8e06ea5f6
SamuelJ <samuel.jimoh@outlook.com>
2018-11-20 21:06:18 -0500

    create user routes for auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/escapade'
+  mongoURI: 'mongodb://localhost:27017/escapade',
+  secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+// const profile = require('./routes/api/profile');
+// const posts = require('./routes/api/posts');
+// const activity = require('./routes/api/activity');
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -14,6 +24,18 @@ mongoose
   )
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/users', users);
+// app.use('/api/profile', profile);
+// app.use('/api/posts', posts);
+// app.use('/api/activity', activity);
 
 const port = process.env.PORT || 5000;
 
